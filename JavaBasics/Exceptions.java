f3bdbf1e5d239070f686bccb20e2abb5865447a9
Rohith Gangaraju <72568522+rohith274@users.noreply.github.com>
2022-01-23 09:50:12 +0530

    Add files via upload

---
+++ JavaBasics/Exceptions.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;// library for scanner class
+
+class Exceprtions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(); //input number
+        System.out.println("You entered : " + a); // print the number
+
+    }
+}
